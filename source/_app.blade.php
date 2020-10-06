e0c19d2f1c6b944ad871ebb7fa21c52c61615747
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-10-06 18:58:14 -0400

    Prod coming soon

--- source/_app.blade.php
+++ source/_app.blade.php
@@ -9,8 +9,12 @@
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" defer></script>
     </head>
-    <body>
-        @yield('body')
+    <body class="flex items-center justify-center min-h-screen bg-gray-100">
+        @if ($page->production)
+            <h1 class="text-xl font-sans font-medium">Coming soon.</h1>
+        @else
+            @yield('body')
+        @endif
         <script src="{{ mix('js/main.js', 'assets/build') }}" defer></script>
         <script src="https://cdn.usefathom.com/script.js" site="QUJVNCEM" included-domains="hannahbarrie.ca" defer></script>
     </body>
