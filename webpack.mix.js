dd45ef69f3679512ae1d41b432a9b0b124f6e3a4
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-03-05 00:38:25 -0500

    Set up React custom preview components

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,5 +7,6 @@ mix.setPublicPath('source/assets/build');
 mix.jigsaw()
     .browserSync({ open: false, server: 'build_local', files: ['build_*/**'] })
     .js('resources/js/main.js', 'js')
+    .js('resources/js/cms.js', 'js').react()
     .postCss('resources/css/main.css', 'css', [require('tailwindcss')])
     .version();
