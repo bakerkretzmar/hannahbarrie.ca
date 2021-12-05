81b9c4767e587d9a4fc1cfce87786d93ebb55ea3
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-12-05 15:33:57 -0500

    Tweak spacing and styles

--- content/_components/menu.blade.php
+++ content/_components/menu.blade.php
@@ -18,7 +18,7 @@
     </ul>
 </nav> --}}
 
-<nav class="flex flex-col gap-4 py-12">
+<nav class="flex flex-col gap-4 py-6 pb-8">
     <h1 class="font-serif text-2xl xs:text-4xl sm:text-5xl">
         <a href="/" class="p-1 link">{{ $page->site_title }}</a>
     </h1>
