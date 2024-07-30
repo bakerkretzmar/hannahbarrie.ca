ec133a9a30cda74833a8c37dc777ceb32d9f38f3
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2024-07-29 20:25:23 -0400

    Wip

--- content/_components/menu.blade.php
+++ content/_components/menu.blade.php
@@ -1,23 +1,3 @@
-{{-- <nav x-data="{ show: false }" class="flex flex-col xs:hidden py-8" x-bind:class="show ? 'pb-4' : ''">
-    <div>
-        <h1 class="font-serif text-2xl">
-            <a href="/" class="p-1 link">{{ $page->site_title }}</a>
-        </h1>
-        <button x-on:click="show = !show" type="button" class="link">
-            <svg x-show="show" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-            <svg x-show="!show" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
-        </button>
-    </div>
-    <ul x-cloak x-show="show" class="flex flex-col items-end py-2 space-y-1">
-        @foreach ($pages as $p)
-            @continue($p->title === 'Home')
-            <li>
-                <a href="{{ $p->getUrl() }}" class="block p-1 link {{ $p->title === $page->title ? 'underline' : '' }}">{{ $p->title }}</a>
-            </li>
-        @endforeach
-    </ul>
-</nav> --}}
-
 <nav class="flex flex-col gap-4 py-6 pb-8">
     <h1 class="font-serif text-2xl xs:text-4xl sm:text-5xl">
         <a href="/" class="p-1 link">{{ $page->site_title }}</a>
--- content/_layouts/app.blade.php
+++ content/_layouts/app.blade.php
@@ -7,7 +7,6 @@
         <title>{{ $page->site_title }} | {{ $page->title }}</title>
         <meta name="description" content="{{ $page->description }}">
         <link rel="canonical" href="{{ $page->getUrl() }}">
-        <link rel="stylesheet" href="https://use.typekit.net/inu3qcv.css">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
         <script defer src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
@@ -16,7 +15,6 @@
     <body class="flex flex-col items-center min-h-screen font-sans xs:text-lg bg-cream">
         <div class="w-full max-w-3xl px-6 sm:px-20">
             <x-menu :page="$page" :pages="$pages" />
-
             <main class="space-y-4 sm:space-y-6 pb-8 sm:pb-16 mx-auto">
                 <section class="prose space-y-4 sm:space-y-6 font-light">
                     @yield('content')
